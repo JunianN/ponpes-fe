0b414826e52057882650cee5c777b1e1ffd962be
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-08-10 14:40:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { SeeSantri } from "@/pages/admin";
 import { SignIn } from "@/pages/auth";
+import { SeeSarpras } from "@/pages/admin";
+import { SeeStruktur } from "@/pages/admin";
+import { SeeUstaz } from "@/pages/admin";
 import { ErrorPage } from "@/pages/Error";
-import { Home } from "@/pages/Home";
-// import { MyBooks } from "@/pages/MyBooks";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/" element={<SeeSantri />} />
+        <Route path="/sarpras" element={<SeeSarpras />} />
+        <Route path="/ustaz" element={<SeeUstaz />} />
+        <Route path="/struktur" element={<SeeStruktur />} />
         <Route
           path="*"
           element={<ErrorPage status="404" statusCode="Page Not Found" />}
